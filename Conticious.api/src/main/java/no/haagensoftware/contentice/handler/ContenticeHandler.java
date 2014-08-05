a6ee2fd30b3f8ca303fe318d90019e525201c1cc
Joachim Haagen Skeie <joachim@haagen-software.no>
2014-08-05 16:41:36 +0200

    - Renaming Contentice to Conticious - Enabling Subcategory renaming and deletion in admin - Adding animation effects in Admin

---
+++ Conticious.api/src/main/java/no/haagensoftware/contentice/handler/ContenticeHandler.java
@@ -0,0 +1,304 @@
+package no.haagensoftware.contentice.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.stream.ChunkedFile;
+import io.netty.util.CharsetUtil;
+import no.haagensoftware.contentice.data.Domain;
+import no.haagensoftware.contentice.spi.AuthenticationPlugin;
+import no.haagensoftware.contentice.spi.StoragePlugin;
+import no.haagensoftware.contentice.util.PluginResolver;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joahaage
+ * Date: 17.11.13
+ * Time: 12:24
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class ContenticeHandler extends SimpleChannelInboundHandler<FullHttpRequest> implements ContenticeParameterMap {
+
+    private Map<String, String> parameterMap;
+    private List<String> queryStringIds;
+    private String browserHost;
+    private Domain domain;
+    private AuthenticationPlugin authenticationPlugin;
+    private PluginResolver pluginResolver;
+    private StoragePlugin storage;
+
+    @Override
+    public String getParameter(String key) {
+        String value = null;
+
+        if (parameterMap != null) {
+            value = parameterMap.get(key);
+        }
+
+        return value;
+    }
+
+    @Override
+    public void setParameterMap(Map<String, String> parameterMap) {
+        this.parameterMap = parameterMap;
+    }
+
+    @Override
+    public List<String> getQueryStringIds() {
+        return queryStringIds;
+    }
+
+    @Override
+    public void setQueryStringIds(List<String> queryStringids) {
+        this.queryStringIds = queryStringids;
+    }
+
+    @Override
+    public PluginResolver getPluginResolver() {
+        return pluginResolver;
+    }
+
+    @Override
+    public void setPluginResolver(PluginResolver pluginResolver) {
+        this.pluginResolver = pluginResolver;
+    }
+
+    public AuthenticationPlugin getAuthenticationPlugin() {
+        return authenticationPlugin;
+    }
+
+    public void setAuthenticationPlugin(AuthenticationPlugin authenticationPlugin) {
+        this.authenticationPlugin = authenticationPlugin;
+    }
+
+    public Domain getDomain() {
+        return domain;
+    }
+
+    public void setDomain(Domain domain) {
+        this.domain = domain;
+    }
+
+    public void setStorage(StoragePlugin storage) {
+        this.storage = storage;
+    }
+
+    public StoragePlugin getStorage() {
+        return storage;
+    }
+
+    public String getUri(FullHttpRequest fullHttpRequest) {
+        return fullHttpRequest.getUri();
+    }
+
+    public String getContentType(FullHttpRequest fullHttpRequest) {
+        String contentType = null;
+
+        HttpHeaders httpHeaders = fullHttpRequest.headers();
+        contentType = httpHeaders.get("Content-Type");
+
+        return contentType;
+    }
+
+    public String getHost(FullHttpRequest fullHttpRequest) {
+        String host = null;
+
+        HttpHeaders httpHeaders = fullHttpRequest.headers();
+        host = httpHeaders.get("Host");
+
+        if (host.contains(":")) {
+            host = host.split(":")[0];
+        }
+
+        return host;
+    }
+
+
+
+    public String getCookieValue(FullHttpRequest fullHttpRequest, String cookieName) {
+        String cookieValue = null;
+
+        HttpHeaders httpHeaders = fullHttpRequest.headers();
+
+        String value = httpHeaders.get("Cookie");
+
+        if (value != null) {
+
+            Set<Cookie> cookies = CookieDecoder.decode(value);
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(cookieName)) {
+                    cookieValue = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        return cookieValue;
+    }
+
+    public boolean isPut(FullHttpRequest fullHttpRequest) {
+        HttpMethod method = fullHttpRequest.getMethod();
+        return method == HttpMethod.PUT;
+    }
+
+    public boolean isPost(FullHttpRequest fullHttpRequest) {
+        HttpMethod method = fullHttpRequest.getMethod();
+        return method == HttpMethod.POST;
+    }
+
+    public boolean isGet(FullHttpRequest fullHttpRequest) {
+        HttpMethod method = fullHttpRequest.getMethod();
+        return method == HttpMethod.GET;
+    }
+
+    public boolean isDelete(FullHttpRequest fullHttpRequest) {
+        HttpMethod method = fullHttpRequest.getMethod();
+        return method == HttpMethod.DELETE;
+    }
+
+    public String getHttpMessageContent(FullHttpRequest fullHttpRequest) {
+        String requestContent = null;
+        ByteBuf content = fullHttpRequest.content();
+        if (content.isReadable()) {
+            requestContent = content.toString(CharsetUtil.UTF_8);
+        }
+        return requestContent;
+    }
+
+    public void storeContentAsPng(FullHttpRequest fullHttpRequest) throws IOException {
+        ByteBuf content = fullHttpRequest.content();
+
+        FileOutputStream out = new FileOutputStream(new File("/Users/jhsmbp/Projects/TeknologihusetWeb/site/uploads/test.png"));
+
+        if (content.isReadable()) {
+            content.readBytes(out, content.readableBytes());
+        }
+    }
+
+    public void handleIncomingRequest(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+
+    }
+
+    public void writeContentsToBuffer(ChannelHandlerContext ctx, String responseText, String contentType) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(responseText.getBytes()));
+        response.headers().set(CONTENT_TYPE, contentType + "; charset=UTF-8");
+        response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+        response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public void writeFileToBuffer(ChannelHandlerContext ctx, String path, String contentType) {
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+        response.headers().set(CONTENT_TYPE, contentType);
+        //response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+
+        File file = new File(path);
+
+        RandomAccessFile raf;
+        try {
+            raf = new RandomAccessFile(file, "r");
+        } catch (FileNotFoundException fnfe) {
+            sendError(ctx, NOT_FOUND);
+            return;
+        }
+        long fileLength = 0;
+        try {
+            fileLength = raf.length();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        //response.headers().set(CONTENT_LENGTH, fileLength);
+        response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+        ctx.write(response);
+
+        ChannelFuture sendFileFuture = null;
+        try {
+            sendFileFuture = ctx.write(new ChunkedFile(raf, 0, fileLength, 8192), ctx.newProgressivePromise());
+            sendFileFuture.addListener(new ChannelProgressiveFutureListener() {
+
+                @Override
+                public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) {
+                    /*if (total < 0) { // total unknown
+                        System.err.println("Transfer progress: " + progress);
+                    } else {
+                        System.err.println("Transfer progress: " + progress + " / " + total);
+                    }*/
+                }
+
+                @Override
+                public void operationComplete(ChannelProgressiveFuture future) throws Exception {
+                    //System.err.println("Transfer complete.");
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        /*if (raf != null) {
+            try {
+                raf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }*/
+
+        // Write the end marker
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public void write404ToBuffer(ChannelHandlerContext ctx) {
+        HttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND, Unpooled.copiedBuffer("404 Not Found", CharsetUtil.UTF_8));
+        ctx.write(response);
+        ctx.flush();
+
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public void writeOKToBuffer(ChannelHandlerContext ctx) {
+        HttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.copiedBuffer("", CharsetUtil.UTF_8));
+        ctx.write(response);
+        ctx.flush();
+
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public void write404DomainNotFoundToBuffer(ChannelHandlerContext ctx) {
+        HttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND, Unpooled.copiedBuffer("That domain is not configured in the Conticious administration panel", CharsetUtil.UTF_8));
+        ctx.write(response);
+        ctx.flush();
+
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+
+    protected void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+        HttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, status, Unpooled.copiedBuffer("Failure: " + status.toString(), CharsetUtil.UTF_8));
+        response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
+
+        // Close the connection as soon as the error message is sent.
+        ctx.write(response);
+        ctx.flush();
+
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+}
---
+++ Conticious.api/src/main/java/no/haagensoftware/contentice/handler/FileServerHandler.java
@@ -0,0 +1,349 @@
+package no.haagensoftware.contentice.handler;
+
+
+import com.sun.management.UnixOperatingSystemMXBean;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import no.haagensoftware.contentice.data.Domain;
+import no.haagensoftware.contentice.util.ContentTypeUtil;
+import no.haagensoftware.conticious.scriptcache.ScriptCache;
+import no.haagensoftware.conticious.scriptcache.ScriptFile;
+import no.haagensoftware.conticious.scriptcache.ScriptHash;
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.activation.MimetypesFileTypeMap;
+import java.io.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A file server that can serve files from file system and class path.
+ *
+ * If you wish to customize the error message, please sub-class and override sendError().
+ * Based on Trustin Lee's original file serving example
+ */
+public class FileServerHandler extends ContenticeHandler {
+    private static final Logger logger = Logger.getLogger(FileServerHandler.class.getName());
+
+    private static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
+
+    protected String rootPath;
+
+    private String stripFromUri;
+    private int cacheMaxAge = -1;
+    private boolean fromClasspath = false;
+    private boolean isAdmin = false;
+    private MimetypesFileTypeMap fileTypeMap = new MimetypesFileTypeMap();
+
+    private int maxCacheSeconds = 0;
+
+    public FileServerHandler() {
+        if (System.getProperty("no.haagensoftware.contentice.webappDir") != null && System.getProperty("no.haagensoftware.contentice.webappDir").length() > 3) {
+            rootPath = System.getProperty("no.haagensoftware.contentice.webappDir");
+        }
+    }
+
+    public void setFromClasspath(boolean fromClasspath) {
+        this.fromClasspath = fromClasspath;
+    }
+
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    protected String sanitizeUri(String uri) throws URISyntaxException {
+        // Decode the path.
+        try {
+            uri = URLDecoder.decode(uri, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            try {
+                uri = URLDecoder.decode(uri, "ISO-8859-1");
+            } catch (UnsupportedEncodingException e1) {
+                throw new Error();
+            }
+        }
+
+        // Convert file separators.
+        uri = uri.replace(File.separatorChar, '/');
+
+        // Simplistic dumb security check.
+        // You will have to do something serious in the production environment.
+        if (uri.contains(File.separator + ".") ||
+            uri.contains("." + File.separator) ||
+            uri.startsWith(".") || uri.endsWith(".")) {
+            return null;
+        }
+
+        QueryStringDecoder decoder = new QueryStringDecoder(uri);
+        uri = decoder.path();
+
+        if (uri.endsWith("/")) {
+            uri += "index.html";
+        } else if (uri.startsWith("/static/") && !uri.endsWith(".html")) {
+            uri = uri + ".html";
+        } else if (uri.startsWith("/static") && !uri.endsWith(".html")) {
+            uri = uri + "/index.html";
+        }
+
+        return uri;
+    }
+
+    public static long getOpenFileDescriptorCount() {
+        OperatingSystemMXBean osStats = ManagementFactory.getOperatingSystemMXBean();
+        if(osStats instanceof UnixOperatingSystemMXBean) {
+            return ((UnixOperatingSystemMXBean)osStats).getOpenFileDescriptorCount();
+        }
+
+        return 0;
+    }
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+
+        logger.info("FileServerHandler channelRead0: " + rootPath + " File handlers before: " + FileServerHandler.getOpenFileDescriptorCount());
+
+        if (!fullHttpRequest.getDecoderResult().isSuccess()) {
+            sendError(channelHandlerContext, HttpResponseStatus.BAD_REQUEST);
+            return;
+        }
+
+        if (!isGet(fullHttpRequest)) {
+            sendError(channelHandlerContext, HttpResponseStatus.METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        String uri = fullHttpRequest.getUri();
+        if (stripFromUri != null) {
+            uri = uri.replaceFirst(stripFromUri, "");
+        }
+
+        String path = sanitizeUri(uri);
+        if (path == null) {
+            sendError(channelHandlerContext, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+
+        logger.info("path: " + path);
+
+        logger.info("//FileServerHandler channelRead0: " + rootPath + " File handlers before: " + FileServerHandler.getOpenFileDescriptorCount());
+
+        String fileContent = "";
+        if (fromClasspath) {
+            URL fileUrl = this.getClass().getResource(path);
+            File file = new File(fileUrl.getPath());
+            if (file.isDirectory()) {
+                path = path + File.separatorChar + "index.html";
+            }
+
+            InputStream in = null;
+            try {
+                in = this.getClass().getResourceAsStream(path);
+                fileContent = convertStreamToString(in);
+            } finally {
+                if (in != null) {
+                    in.close();
+                }
+            }
+
+            writeContentsToBuffer(channelHandlerContext, fileContent.toString(), ContentTypeUtil.getContentType(path));
+        } else {
+            String originalPath = path;
+
+            String webappName = null;
+
+            Domain domain = getDomain();
+
+            if (domain == null && originalPath.startsWith("/admin")) {
+                webappName = "admin";
+            } else if (domain != null) {
+                webappName = domain.getWebappName();
+            }
+
+            if (webappName == null) {
+                write404DomainNotFoundToBuffer(channelHandlerContext);
+            } else {
+                if (webappName != null && !isAdmin) {
+                    path = "/" + webappName + path;
+                }
+
+                File file = new File(this.rootPath + path);
+
+                File staticFile = new File(rootPath + File.separatorChar + webappName + File.separatorChar + "static" + File.separatorChar + originalPath);
+
+                if (!file.exists()) {
+                    path = rootPath + File.separatorChar + webappName + File.separatorChar + "index.html";
+                } else if (file.isDirectory()) {
+                    path = this.rootPath + path + File.separatorChar + "index.html";
+                } else {
+                    path = this.rootPath + path;
+                }
+
+                String staticPath = staticFile.getAbsolutePath();
+
+                if (staticFile.isDirectory() || !staticFile.exists()) {
+                    staticPath = staticFile.getAbsolutePath() + ".html";
+                }
+
+                staticFile = new File(staticPath);
+
+                logger.info("path: " + path);
+                logger.info("original path: " + originalPath);
+                logger.info("static path: " + staticPath);
+
+                if (path.endsWith(".html")) {
+                    ScriptCache cache = ScriptHash.getScriptCache(path);
+                    if (cache == null || cache.isExpired()) {
+                        //If file is not cached, or cache is expired, update the cache.
+                        logger.info("Updating index.html from filesystem path: " + path);
+                        cache = updateScriptCacheForPath(getDomain().getWebappName(), path);
+                    }
+
+                    String htmlContents = cache.getHtmlContents();
+
+                    //Add static contents inside a NOSCRIPT tag
+                    if (path.endsWith(".html") && staticFile.exists() && staticFile.isFile()) {
+                        logger.info("Adding noscript tag");
+
+                        Document htmlDocument = Jsoup.parse(htmlContents, "UTF-8");
+                        Document staticDocument = parseHtmlPage(staticFile.getAbsolutePath());
+
+                        logger.info(htmlDocument.toString());
+                        logger.info("\n\n---\n\n");
+
+                        Element noscript = htmlDocument.body().appendElement("noscript");
+
+                        for (Element element : staticDocument.getElementsByTag("body").get(0).children()) {
+                            noscript.appendChild(element);
+                        }
+
+                        htmlContents = htmlDocument.toString();
+                    }
+
+                    writeContentsToBuffer(channelHandlerContext, htmlContents, ContentTypeUtil.getContentType(path));
+                } else {
+                    writeFileToBuffer(channelHandlerContext, path, ContentTypeUtil.getContentType(path));
+                }
+            }
+        }
+    }
+
+    private ScriptCache updateScriptCacheForPath(String host, String path) throws IOException {
+        Long before = System.currentTimeMillis();
+        List<ScriptFile> scriptPathList = new ArrayList<ScriptFile>();
+
+        Document htmlDocument = parseHtmlPage(path);
+
+        //extract out the JavaScript tags with src attribute and replace with a single
+        //call to a cached minified script file
+        if (htmlDocument != null) {
+            Elements elements = htmlDocument.select("body");
+            Element headElement = elements.get(0);
+
+            Elements scriptElements = headElement.getElementsByTag("script");
+            for (Element scriptElement : scriptElements) {
+                String scriptSrc = scriptElement.attr("src");
+                if (scriptSrc == null || scriptSrc.startsWith("http")) {
+                    //Keep the script as-is
+                } else if (scriptSrc != null && scriptSrc.endsWith(".js")) {
+                    File minifiedScriptFile = new File(rootPath + File.separatorChar + host + File.separatorChar + scriptSrc.substring(0, scriptSrc.length() - 3) + "min.js");
+                    String fileContent = null;
+
+                    if (minifiedScriptFile != null && minifiedScriptFile.isFile()) {
+                        fileContent = getFileContent(minifiedScriptFile.getAbsolutePath());
+                    } else {
+                        fileContent = getFileContent(rootPath + File.separatorChar +  host + File.separatorChar + scriptSrc);
+                    }
+
+                    //cache and remove this <script src tag from the DOM
+                    if (fileContent != null) {
+                        scriptPathList.add(new ScriptFile(scriptSrc, fileContent));
+                    }
+                    scriptElement.remove();
+                }
+            }
+
+            //Append a new script element to the head-tag representing the cached and
+            //minified script
+            int randomnum = (int)(Math.random() * 1000d);
+
+            headElement.appendElement("script")
+                    .attr("src", "/cachedScript" + path + ".js?" + "id=" + randomnum)
+                    .attr("type", "text/javascript")
+                    .attr("charset", "utf-8");
+        }
+
+        //Create or update the script contents for this HTML file path
+        ScriptCache cache = ScriptHash.updateScriptContents(path, scriptPathList, htmlDocument.html(), System.currentTimeMillis() + (maxCacheSeconds * 1000));
+        logger.info("Finished extracting script contents took: " + (System.currentTimeMillis() - before) + " ms.");
+
+        return cache;
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        if (ctx.channel().isActive()) {
+            sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private static String convertStreamToString(java.io.InputStream is) {
+        if (is != null) {
+            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+            return (s != null && s.hasNext()) ? s.next() : "";
+        }
+
+        return "";
+    }
+
+    protected String getFileContent(String path) {
+        String fileContent = null;
+        InputStream is;
+        try {
+            if (fromClasspath) {
+                is = this.getClass().getResourceAsStream(path);
+            } else {
+                is = new FileInputStream(path);
+            }
+
+            if (is == null) {
+                return null;
+            }
+
+            fileContent = convertStreamToString(is);
+
+        } catch (IOException e) {
+            return null;
+        }
+
+        return fileContent;
+    }
+
+    /**
+     * Simple method for parsing the HTML contents
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    private Document parseHtmlPage(String path) throws IOException {
+        Document htmlDocument = null;
+
+        File input = new File(path);
+        if (input != null && input.exists() && input.isFile()) {
+            htmlDocument = Jsoup.parse(input, "UTF-8");
+        }
+        return htmlDocument;
+    }
+}
