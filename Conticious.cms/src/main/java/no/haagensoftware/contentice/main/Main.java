112995865dd69b71464e9661fcc797447cff6199
Joachim Haagen Skeie <joachim@haagen-software.no>
2014-08-13 11:21:21 +0200

    Adding startup and shutdown methods to Main

--- Conticious.cms/src/main/java/no/haagensoftware/contentice/main/Main.java
+++ Conticious.cms/src/main/java/no/haagensoftware/contentice/main/Main.java
@@ -32,6 +32,9 @@ import java.util.Properties;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
     private Integer port;
+    private ServerBootstrap bootstrap;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -106,16 +109,18 @@ public class Main {
     public void bootstrap() throws Exception {
         PluginResolver pluginResolver = new PluginResolver();
 
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.option(ChannelOption.SO_BACKLOG, 1024);
-            b.group(bossGroup, workerGroup)
+            bootstrap = new ServerBootstrap();
+            bootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+            bootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ContenticePipelineInitializer(pluginResolver));
 
-            Channel ch = b.bind(port).sync().channel();
+            logger.info("Binding to port: " + port);
+
+            Channel ch = bootstrap.bind(port).sync().channel();
             ch.closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
@@ -123,33 +128,52 @@ public class Main {
         }
     }
 
+    public void shutdown() {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+        bootstrap.childGroup().shutdownGracefully();
+
+        bossGroup = null;
+        workerGroup = null;
+        bootstrap = null;
+    }
+
     private void readProperties() throws IOException {
-        Properties properties = new Properties();
-        File configFile = new File("config.properties");
-        if (!configFile.exists()) {
-            logger.info("config.properties not found at : " + configFile.getAbsolutePath() + " trying one level up.");
-            configFile = new File("../config.properties");
-        }
-        if (!configFile.exists()) {
-            logger.info("config.properties not found at : " + configFile.getAbsolutePath() + " trying one level up.");
-            configFile = new File("../../config.properties");
-        }
-        if (configFile.exists()) {
-            FileInputStream configStream = new FileInputStream(configFile);
-            properties.load(configStream);
-            configStream.close();
-            logger.info("Server properties loaded from " + configFile.getAbsolutePath());
-            for (Enumeration<Object> e = properties.keys(); e.hasMoreElements();) {
-                Object property = (String) e.nextElement();
-                logger.info("\t\t* " + property + "=" + properties.get(property));
-            }
+        String startWithoutConfigfile = System.getProperty("no.conticious.startWithoutConfigProperties");
+
+        if (startWithoutConfigfile != null && startWithoutConfigfile.equalsIgnoreCase("true")) {
+            //Do nothing. This is only useful if you are bootstrapping manually, like with the ContriciousLocal project.
         } else {
-            String message = "Could not find " + configFile.getAbsolutePath() + ". Unable to start.";
-            logger.error(message);
-            throw new RuntimeException(message);
+
+            Properties properties = new Properties();
+            File configFile = new File("config.properties");
+            if (!configFile.exists()) {
+                logger.info("config.properties not found at : " + configFile.getAbsolutePath() + " trying one level up.");
+                configFile = new File("../config.properties");
+            }
+            if (!configFile.exists()) {
+                logger.info("config.properties not found at : " + configFile.getAbsolutePath() + " trying one level up.");
+                configFile = new File("../../config.properties");
+            }
+            if (configFile.exists()) {
+                FileInputStream configStream = new FileInputStream(configFile);
+                properties.load(configStream);
+                configStream.close();
+                logger.info("Server properties loaded from " + configFile.getAbsolutePath());
+                for (Enumeration<Object> e = properties.keys(); e.hasMoreElements(); ) {
+                    Object property = (String) e.nextElement();
+                    logger.info("\t\t* " + property + "=" + properties.get(property));
+                }
+            } else {
+                String message = "Could not find " + configFile.getAbsolutePath() + ". Unable to start.";
+                logger.error(message);
+                throw new RuntimeException(message);
+            }
+
+            setProperties(properties);
         }
 
-        setProperties(properties);
+        port = Integer.parseInt(System.getProperty("no.haagensoftware.contentice.port", "8080"));
     }
 
     private void setProperties(Properties properties) {
@@ -168,13 +192,6 @@ public class Main {
         if (properties.get("no.haagensoftware.contentice.storage.plugin") == null) {
             properties.setProperty("no.haagensoftware.contentice.storage.plugin", "FileSystemStoragePlugin");
             logger.warn("Storage Plugin is missing from configuration. Using default 'FileSystemStoragePlugin' plugin");
-        }
-
-        port = Integer.parseInt(properties.getProperty("no.haagensoftware.contentice.port"));
-        Enumeration<Object> propEnum = properties.keys();
-        while (propEnum.hasMoreElements()) {
-            String property = (String) propEnum.nextElement();
-            System.setProperty(property, properties.getProperty(property));
         }
     }
 
